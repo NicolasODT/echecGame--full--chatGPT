b4f3bb5f5a4cf084e7b9a3f6a3e6c7bc45695c82
Nicolas Terwagne <123467708+NicolasODT@users.noreply.github.com>
2023-04-07 21:17:22 +0200

    Update echecs.py

--- echecs.py
+++ echecs.py
@@ -78,6 +78,24 @@ def coordonnees_pixel_vers_case(pos):
     return x // taille_case, y // taille_case
 
 
+def afficher_resultat(manager, resultat):
+    message = ""
+    if resultat == "1-0":
+        message = "Vous avez gagné !"
+    elif resultat == "0-1":
+        message = "Vous avez perdu."
+    elif resultat == "1/2-1/2":
+        message = "Match nul."
+    else:
+        message = "Erreur : résultat inattendu."
+    
+    if message:
+        resultat_dialog = UIConfirmationDialog(rect=pygame.Rect((taille_echiquier // 2 - 100, taille_echiquier // 2 - 50), (200, 100)), manager=manager, window_title="Résultat", action_long_desc=message, blocking=True)
+        restart_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((taille_echiquier // 2 - 50, taille_echiquier // 2 + 20), (100, 30)), text="Redémarrer", manager=manager)
+        return restart_button
+
+
+
 def main():
     screen = pygame.display.set_mode((taille_echiquier + 200, taille_echiquier))
     pygame.display.set_caption("Jeu d'échecs")
@@ -93,6 +111,9 @@ def main():
 
     clock = pygame.time.Clock()
 
+    restart_dialog = None
+    restart_result_button = None
+
     while running and not echiquier.is_game_over():
         time_delta = clock.tick(60) / 1000.0
 
@@ -100,30 +121,28 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-            manager.process_events(event)  # Déplacez cette ligne ici
+            manager.process_events(event)
 
             # Gestion des événements de l'interface utilisateur
             if event.type == pygame.USEREVENT:
-                # Remplacez ce bloc de code dans la boucle des événements:
-                if event.type == pygame.USEREVENT:
-                    if event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:  # Utilisez 'user_type' au lieu de 'type'
-                        if event.ui_element == difficulty_dropdown:
-                            restart_dialog = UIConfirmationDialog(rect=pygame.Rect((taille_echiquier // 2 - 100, taille_echiquier // 2 - 50), (200, 100)), manager=manager, window_title="Confirmation", action_long_desc="Voulez-vous redémarrer la partie avec le nouveau niveau de difficulté ?", blocking=True)
-                    elif event.user_type == pygame_gui.UI_BUTTON_PRESSED:  # Utilisez 'user_type' au lieu de 'type'
-                        if event.ui_element == restart_button:
-                            restart_dialog = UIConfirmationDialog(rect=pygame.Rect((taille_echiquier // 2 - 100, taille_echiquier // 2 - 50), (200, 100)), manager=manager, window_title="Confirmation", action_long_desc="Voulez-vous redémarrer la partie ?", blocking=True)
-                    elif event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:  # Utilisez 'user_type' au lieu de 'type'
-                        if restart_dialog:
-                            echiquier = chess.Board()
-                            piece_selectionnee = None
-                            restart_dialog = None
+                if event.type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+                    if event.ui_element == difficulty_dropdown:
+                        restart_dialog = UIConfirmationDialog(rect=pygame.Rect((taille_echiquier // 2 - 100, taille_echiquier // 2 - 50), (200, 100)), manager=manager, window_title="Confirmation", action_long_desc="Voulez-vous redémarrer la partie avec le nouveau niveau de difficulté ?", blocking=True)
+                elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == restart_button:
+                        restart_dialog = UIConfirmationDialog(rect=pygame.Rect((taille_echiquier // 2 - 100, taille_echiquier // 2 - 50), (200, 100)), manager=manager, window_title="Confirmation", action_long_desc="Voulez-vous redémarrer la partie ?", blocking=True)
+                elif event.type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
+                    if restart_dialog:
+                        echiquier = chess.Board()
+                        piece_selectionnee = None
+                        restart_dialog = None
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # Bouton gauche de la souris
+                if event.button == 1:
                     x, y = coordonnees_pixel_vers_case(event.pos)
 
                     if piece_selectionnee:
-                        if piece_selectionnee == (x, y):  # La même pièce a été sélectionnée à nouveau
+                        if piece_selectionnee == (x, y):
                             piece_selectionnee = None
                         else:
                             mouvement = chess.Move.from_uci(chess.square_name(8 * (7 - piece_selectionnee[1]) + piece_selectionnee[0]) + chess.square_name(8 * (7 - y) + x))
@@ -131,18 +150,20 @@ def main():
                             if mouvement in echiquier.legal_moves:
                                 echiquier.push(mouvement)
                                 piece_selectionnee = None
+                                if echiquier.is_game_over():
+                                    restart_result_button = afficher_resultat(manager, echiquier.result())
                             elif echiquier.piece_at(8 * (7 - y) + x) and echiquier.color_at(8 * (7 - y) + x) == echiquier.turn:
                                 piece_selectionnee = (x, y)
                     else:
                         if echiquier.piece_at(8 * (7 - y) + x) and echiquier.color_at(8 * (7 - y) + x) == echiquier.turn:
                             piece_selectionnee = (x, y)
-
-
-        if echiquier.turn == chess.BLACK:
-            profondeur_dict = {"Facile": 1, "Moyen": 3, "Difficile": 5}
-            profondeur = profondeur_dict[difficulty_dropdown.selected_option]
-            mouvement_bot = choisir_mouvement_bot(echiquier)  # Supprimez l'argument 'profondeur' ici
-            echiquier.push(mouvement_bot)
+            if echiquier.turn == chess.BLACK:
+                profondeur_dict = {"Facile": 1, "Moyen": 3, "Difficile": 5}
+                profondeur = profondeur_dict[difficulty_dropdown.selected_option]
+                mouvement_bot = choisir_mouvement_bot(echiquier)
+                echiquier.push(mouvement_bot)
+                if echiquier.is_game_over():
+                    restart_result_button = afficher_resultat(manager, echiquier.result())
 
         screen.fill((0, 0, 0))
         afficher_echiquier(screen, echiquier)
